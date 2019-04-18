24d4365d345bbd2193529c7d83b360a092169d2e
Jonathan Liu <liu.z.jonathan@gmail.com>
2019-04-18 10:15:32 -0700

    Reorganized program
    Added comments, made more readable and such

--- tothebeat.py
+++ tothebeat.py
@@ -1,47 +1,12 @@
 # Note: Must have ffmpeg installed for this to work
 
-import numpy as np
-
 import librosa
-import librosa.display
-
-import matplotlib.pyplot as plt
-
 import subprocess
-
 import sys
 from os import walk
-import os
-import tempfile
-
-import time
-'''
-audio_path = './training_data/summer.mp3'
-
-y, sr = librosa.load(audio_path)
-
-y_harmonic, y_percussive = librosa.effects.hpss(y)
-
-tempo, beat_frames = librosa.beat.beat_track(y=y_percussive, sr=sr)
-
-print('Tempo: {:.2f} bpm'.format(tempo))
-
-beat_times = librosa.frames_to_time(beat_frames, sr=sr)
-
-print(beat_times)
-
-librosa.output.times_csv('summer.csv', beat_times)  
-'''
-beat_times = [0]
-with open('energy.csv') as f:
-    lines = f.readlines()
-    lines = [line for line in lines if line != '\n']
-    for i, row in enumerate(lines):
-        if i % 4 == 0:
-            sec = float(row.strip())
-            beat_times.append(sec)
 
 def getDuration(filename):
+    # Get the duration of media file `filename`
     result = subprocess.Popen(['ffmpeg', '-i', filename], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     for l in result.stdout.readlines():
         l = l.decode('utf-8')
@@ -50,6 +15,7 @@ def getDuration(filename):
     raise Exception('Video length not found for ' + filename)
 
 def getFrameRate(filename):
+    # Get the frame rate of media file `filename`
     result = subprocess.Popen(['ffmpeg', '-i', filename], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     for l in result.stdout.readlines():
         l = l.decode('utf-8')
@@ -58,37 +24,77 @@ def getFrameRate(filename):
     raise Exception('Framerate not found for ' + filename)
 
 def getSec(timestamp):
+    # Get the amount of seconds in a timestamp of format HH:MM:SS
     h, m, s = timestamp.split(':')
     return int(h) * 3600 + int(m) * 60 + int(s)
 
+def getBeatTimesFromMusic(path, split_every_n_beat):
+    # Get the beat times using librosa from an audio file `path`
+    y, sr = librosa.load(path)
+    y_harmonic, y_percussive = librosa.effects.hpss(y)
+    tempo, beat_frames = librosa.beat.beat_track(y=y_percussive, sr=sr)
+    beat_times_init = librosa.frames_to_time(beat_frames, sr=sr)
+
+    beat_times = [0]
+    for i in range(len(beat_times_init)):
+        if i % split_every_n_beat == 0:
+            beat_times.append(beat_times_init[i])
+    beat_times.append(getSec(getDuration(path)))
+
+    return beat_times
+
+def getBeatTimesFromCSV(path, split_every_n_beat):
+    # Get the beat times from a csv file `path`
+    beat_times = [0]
+    with open(path) as f:
+        lines = f.readlines()
+        lines = [line for line in lines if line != '\n']
+        for i, row in enumerate(lines):
+            if i % split_every_n_beat == 0:
+                sec = float(row.strip())
+                beat_times.append(sec)
+    beat_times.append(getSec(getDuration(audio_path)))
+
+    return beat_times
+
+def exportBeatTimesAsCSV(beat_times, path):
+    librosa.output.times_csv(path, beat_times)  
+
+###########
+# OPTIONS #
+###########
+audio_path = './training_data/creativeminds.mp3'    # Path of the song being used
+sep = 5                                             # Clips from the same video must be at least this many seconds apart
+resolution_w = 1920                                 # Output resolution of video (WIDTH)
+resolution_h = 1080                                 # Output resolution of video (HEIGHT)
+fps = 30                                            # Output FPS of video
+vid_directory = sys.argv[1]                         # Directory that stores the videos to edit together
+split_every_n_beat = 8                              # The beat at which clips are split at 
+preset = 'ultrafast'                                # FFMPEG preset to encode the videos
+output_file_name = 'output.mp4'                     # The video file to export
+
+#
+# Get beat_times and the list of videos
+#
+beat_times = getBeatTimesFromMusic(audio_path, split_every_n_beat)
+
 vids = []
-for (dirpath, dirnames, filenames) in walk(sys.argv[1]):
+for (dirpath, dirnames, filenames) in walk(vid_directory):
     vids.extend([dirpath+'/'+name for name in filenames])
 
-print(vids)
-
-
-sep = 5 # Clips from the same video must be at least this many seconds apart
-
-'''
-vid_index = 0
-for i in range(len(beat_times) - 1):
-    interval = beat_times[i+1] - beat_times[i]
-    #print(interval)
-    if (vid_index >= len(vids)):
-        break
-         
-    length = getLength(vids[vid_index])
-'''
-
-resolution_w = 1920
-resolution_h = 1080
-#3840x2160
-fps = 30
+#
+# Create the `clips` list, which stores information to split the videos
+# Each index of clips is organized as such:
+# (video_index, start_frame, end_frame, speed_factor)
+#   video_index : int   - The index of the video in `vids`
+#   start_frame : int   - The frame to start cutting the video
+#   end_frame   : int   - The frame to stop cutting the video
+#   speed_factor: float - The factor by which to multiply the PTS of the video to fix speed issues with forcing the input frame rates to `fps`
+#
 clips = []
+tot_frames = 0
 beat_index = 0
 interval = beat_times[beat_index + 1] - beat_times[beat_index]
-tot_frames = 0
 
 for i in range(len(vids)):
     if beat_index + 1 >= len(beat_times):
@@ -102,10 +108,12 @@ for i in range(len(vids)):
 
     cur_time = 0
 
+    # While still enough time left in the video
     while cur_time+interval < length:
         cur_time_fn = cur_time * fps
-        interval_fn = interval * fps # + err
+        interval_fn = interval * fps
 
+        # Correct the frame number if it doesn't match up with the current beat_time
         correct_frame = int(beat_times[beat_index] * fps)
         if tot_frames != correct_frame:
             interval_fn += correct_frame - tot_frames
@@ -113,23 +121,19 @@ for i in range(len(vids)):
         clips.append((i, int(cur_time_fn), int(cur_time_fn) + int(interval_fn), factor))
         tot_frames += int(interval_fn)
         
-
         cur_time = int(cur_time+interval+sep)
-
         beat_index += 1
+        if beat_index + 1 >= len(beat_times):
+            break
+
         interval = beat_times[beat_index + 1] - beat_times[beat_index]
 
-print(beat_times)
-
-
-print(clips)
-
-#NEW version that uses filter_complex
-song_name = 'training_data/energy.mp3'
-
+#
+# Use filter_complex to combine videos at the specified frame splits 
+#
 cmd = ['ffmpeg']
 cmd.append('-i')
-cmd.append(song_name)
+cmd.append(audio_path)
 
 for i in range(len(vids)):
     cmd.append('-r')
@@ -141,12 +145,20 @@ cmd.append('-filter_complex')
 filter_str = ''
 concat_str = ''
 for i in range(len(clips)):
-    # Add 1 to clips[i][0] because the song is the 0th input
-    trim_str = '[%d:v]setpts=%.3f*PTS,trim=start_pts=%d:end_pts=%d,setpts=PTS-STARTPTS,scale=w=%d:h=%d:force_original_aspect_ratio=decrease[v%d];' % (clips[i][0] + 1, clips[i][3], clips[i][1], clips[i][2], resolution_w, resolution_h, i)
-    #print(trim_str)
+    # Add 1 to stream_num because the song is the 0th input
+    stream_num = clips[i][0] + 1
+    factor = clips[i][3]
+    start_pts = clips[i][1]
+    end_pts = clips[i][2]
+    trim_str = f'[{stream_num}:v]\
+                setpts={factor:.3f}*PTS,\
+                trim=start_pts={start_pts}:end_pts={end_pts},\
+                setpts=PTS-STARTPTS,\
+                scale=w={resolution_w}:h={resolution_h}:force_original_aspect_ratio=decrease\
+                [v{i}];'
     filter_str += trim_str
-    concat_str += '[v%d]' % (i)
-concat_str += 'concat=n=%d[out]' % (len(clips))
+    concat_str += f'[v{i}]'
+concat_str += f'concat=n={len(clips)}[out]'
 filter_str += concat_str
 
 cmd.append(filter_str)
@@ -156,91 +168,11 @@ cmd.append('[out]')
 cmd.append('-map')
 cmd.append('0:a')
 cmd.append('-preset')
-cmd.append('ultrafast')
+cmd.append(preset)
 cmd.append('-r')
 cmd.append(str(fps))
-cmd.append('output.mp4')
+cmd.append('-y')
+cmd.append(output_file_name)
 subprocess.call(cmd)
 
-#OLD version that uses stream copy
-'''
-with tempfile.TemporaryDirectory() as directory:
-    #directory = 'tmp'
-    concat_str = ''
-
-    print('Cutting clips...', end='')
-    sys.stdout.flush()
-
-    for i in range(len(clips)):
-        mp4_name = 'out'+str(i)+'.mp4'
-        cmd = ['ffmpeg', '-hide_banner', '-loglevel', 'panic', '-ss', clips[i][1], '-i', clips[i][0], '-t', clips[i][2], '-avoid_negative_ts', 'make_zero', '-c', 'copy', '-y', directory + '/' + mp4_name]
-        #cmd = ['ffmpeg', '-i', clips[i][0], '-ss', clips[i][1], '-t', clips[i][2], '-c', 'copy', '-y', directory + '/' + mp4_name]
-        subprocess.call(cmd)
-
-        mp4_corrected_name = 'out'+str(i)+'_c.mp4' # Correct the length
-        cmd = ['ffmpeg', '-hide_banner', '-loglevel', 'panic', '-i', directory + '/' + mp4_name, '-t', clips[i][2], '-avoid_negative_ts', 'make_zero', '-c', 'copy', '-y', directory + '/' + mp4_corrected_name]
-        subprocess.call(cmd)
-
-        concat_str += "file '"+ mp4_corrected_name +"'\n"
-
-    concat_file = directory + '/list.txt'
-    with open(concat_file, 'w') as f:
-        f.write(concat_str)
-
-    print('Finished.')
-
-    print('Combining clips...', end='')
-    sys.stdout.flush()
-    full_vid_name = directory+'/full.mp4'
-    cmd = ['ffmpeg', '-hide_banner', '-f', 'concat', '-safe', '0', '-i', concat_file, '-c', 'copy', '-y', full_vid_name]
-    subprocess.call(cmd)
-    print('Finished.')
-
-    print('Adding audio...', end='')
-    sys.stdout.flush()
-    song_name = 'training_data/energy.mp3'
-    cmd = ['ffmpeg', '-hide_banner', '-i', full_vid_name, '-i', song_name, '-c', 'copy', '-shortest', '-map', '0:v:0', '-map', '1:a:0', '-y', 'final.mp4']
-    subprocess.call(cmd)
-    print('Finished')
-
-    print('Temp directory: ', directory)
-'''
-
 print('DONE')
-
-#'-loglevel', 'panic',
-
-    
-
-
-# ('test_videos/dance.mp4', '0', '0.093')    
-
-
-#t = getLength('test_videos/VID_20190308_163349.mp4')
-#if ()
-
-
-''' ### Normal cut
-ffmpeg -i input.mp4 -ss 00:27 -t 00:15 -acodec copy -vcodec copy -y output_nc.mp4
-'''
-
-''' ### Keyframe cut
-ffmpeg -ss 00:27 -i input.mp4 -t 00:15 -avoid_negative_ts make_zero -acodec copy -vcodec copy -y output_kc.mp4
-'''
-
-'''
-ffmpeg -i input.mp4 -c copy int1.ts
-ffmpeg -i "concat:int1.ts|int2.ts" -c copy output.mp4
-'''
-
-'''
-ffmpeg -i output.mp4 -i training_data/energy.mp3 -c copy -shortest -map 0:v:0 -map 1:a:0 final.mp4
-'''
-
-'''
-ffmpeg -i test_videos/VID_20190308_163349.mp4 -filter_complex "
-    [0:v]trim=5:10,setpts=PTS-STARTPTS[v0];
-    [0:v]trim=60:65,setpts=PTS-STARTPTS[v1];
-    [v0][v1]concat=n=2:v=1:a=0[out]
-" -map "[out]" output.mp4
-'''
