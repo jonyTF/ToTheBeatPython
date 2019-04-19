5a9ae903519636f512d3764412e4b2a72633695c
Jonathan Liu <liu.z.jonathan@gmail.com>
2019-04-18 17:44:30 -0700

    worked on gui and progress

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout, QHBoxLayout, QGridLayout, QPushButton, QMainWindow, QGroupBox, QLineEdit, QFileDialog, QListWidget, QSizePolicy, QAbstractItemView
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout, QHBoxLayout, QGridLayout, QPushButton, QMainWindow, QGroupBox, QLineEdit, QFileDialog, QListWidget, QSizePolicy, QAbstractItemView, QProgressBar
+from PyQt5.QtCore import Qt
+import tothebeat
 import sys
 
 # TODO: Allow user to either select a bunch of video files (store this in a list) or just select a directory and add all the video files
@@ -19,6 +21,9 @@ class MainWindow(QMainWindow):
         # Video chooser
         self.vid_chooser_list = QListWidget()
         self.vid_chooser_list.setSelectionMode(QAbstractItemView.ExtendedSelection)
+        self.vid_chooser_model = self.vid_chooser_list.model()
+        self.vid_chooser_model.rowsInserted.connect(self.changeVidChooserBtnState)
+        self.vid_chooser_model.rowsRemoved.connect(self.changeVidChooserBtnState)
 
         self.vid_add_btn = QPushButton('Add')
         self.vid_add_btn.clicked.connect(self.addVideos)
@@ -30,10 +35,13 @@ class MainWindow(QMainWindow):
         self.vid_down_btn.clicked.connect(self.moveVideosDown)
 
         self.vid_chooser_btn_layout = QVBoxLayout()
+        self.vid_chooser_btn_layout.setContentsMargins(0, 0, 0, 0)
+        self.vid_chooser_btn_layout.setSpacing(0)
         self.vid_chooser_btn_layout.addWidget(self.vid_add_btn)
         self.vid_chooser_btn_layout.addWidget(self.vid_rm_btn)
         self.vid_chooser_btn_layout.addWidget(self.vid_up_btn)
         self.vid_chooser_btn_layout.addWidget(self.vid_down_btn)
+        self.vid_chooser_btn_layout.addStretch(0)
 
         self.vid_chooser_layout = QHBoxLayout()
         self.vid_chooser_layout.addWidget(self.vid_chooser_list)
@@ -44,6 +52,7 @@ class MainWindow(QMainWindow):
 
         # Music chooser
         self.music_file_textbox = QLineEdit()
+        self.music_file_textbox.textChanged.connect(self.musicFileTextboxChanged)
         self.music_file_browse_btn = QPushButton('Browse')
         self.music_file_browse_btn.clicked.connect(self.browseMusicFile)
 
@@ -54,15 +63,59 @@ class MainWindow(QMainWindow):
         self.music_chooser_group_box = QGroupBox('Music')
         self.music_chooser_group_box.setLayout(self.music_chooser_layout)
 
+        # Start button (create video?)
+        # Maybe make button bigger vertically so it's more obvious?
+        self.start_btn = QPushButton('Start')
+        self.start_btn.clicked.connect(self.start)
+        self.can_start = {'vid_chooser_list': False, 'music_file_textbox': False}
+        
+        self.progress_bar = QProgressBar()
+        self.progress_bar.setMaximum(100)
+        self.progress_bar.setValue(0)
+        
+        self.changeVidChooserBtnState()
+
         # Add everything to the main layout
         self.layout.addWidget(self.vid_chooser_group_box)
         self.layout.addWidget(self.music_chooser_group_box)
+        self.layout.addWidget(self.start_btn)
+        self.layout.addWidget(self.progress_bar)
 
         self.central_widget.setLayout(self.layout)
         self.setCentralWidget(self.central_widget)
 
         self.show()
+
+    def checkCanStart(self):
+        if self.can_start['vid_chooser_list'] and self.can_start['music_file_textbox']:
+            self.start_btn.setEnabled(True)
+        else:
+            self.start_btn.setEnabled(False)
     
+    def changeVidChooserBtnState(self):
+        if len(self.vid_chooser_list) > 0:
+            self.vid_rm_btn.setEnabled(True)
+            self.vid_up_btn.setEnabled(True)
+            self.vid_down_btn.setEnabled(True)
+
+            self.can_start['vid_chooser_list'] = True
+        else:
+            self.vid_rm_btn.setEnabled(False)
+            self.vid_up_btn.setEnabled(False)
+            self.vid_down_btn.setEnabled(False)
+
+            self.can_start['vid_chooser_list'] = False
+        
+        self.checkCanStart()
+
+    def musicFileTextboxChanged(self):
+        if len(self.music_file_textbox.text()) > 0:
+            self.can_start['music_file_textbox'] = True
+        else:
+            self.can_start['music_file_textbox'] = False
+        
+        self.checkCanStart()
+
     def addVideos(self):
         file_names = QFileDialog.getOpenFileNames(self, 'Select video files', '', 'Video files (*.mp4 *.avi *.mov *.flv *.wmv)')[0]
         self.vid_chooser_list.addItems(file_names)
@@ -94,6 +147,20 @@ class MainWindow(QMainWindow):
     def browseMusicFile(self):
         file_name = QFileDialog.getOpenFileName(self, 'Select a music file', '', 'Audio files (*.3gp *.aa *.aac *.aax *.act *.aiff *.amr *.ape *.au *.awb *.dct *.dss *.dvf *.flac *.gsm *.iklax *.ivs *.m4a *.m4b *.m4p *.mmf *.mp3 *.mpc *.msv *.nmf *.nsf *.ogg *.oga *.mogg *.opus *.ra *.rm *.raw *.sln *.tt *.vox *.wav *.webm *.wma *.wv)')[0]
         self.music_file_textbox.setText(file_name)
+
+    def start(self):
+        output_file_name = QFileDialog.getSaveFileName(self, 'Save video as...', '', 'Video files (*.mp4 *.avi *.mov *.flv *.wmv)')[0]
+        vids = []
+        for i in range(len(self.vid_chooser_list)):
+            vids.append(self.vid_chooser_list.item(i).text())
+ 
+        tothebeat.createVideo(
+            self.music_file_textbox.text(),
+            output_file_name,
+            1920,
+            1080,
+            vids=vids
+        )
 
 if __name__ == '__main__':
     app = QApplication([])
--- tothebeat.py
+++ tothebeat.py
@@ -43,10 +43,10 @@ def getBeatTimesFromMusic(path, split_every_n_beat):
 
     return beat_times
 
-def getBeatTimesFromCSV(path, split_every_n_beat):
+def getBeatTimesFromCSV(csv_path, audio_path, split_every_n_beat):
     # Get the beat times from a csv file `path`
     beat_times = [0]
-    with open(path) as f:
+    with open(csv_path) as f:
         lines = f.readlines()
         lines = [line for line in lines if line != '\n']
         for i, row in enumerate(lines):
@@ -66,116 +66,156 @@ def exportBeatTimesAsCSV(beat_times, path):
 ###########
 # OPTIONS #
 ###########
-audio_path = './music/creativeminds.mp3'            # Path of the song being used
-sep = 5                                             # Clips from the same video must be at least this many seconds apart
-resolution_w = 1920                                 # Output resolution of video (WIDTH)
-resolution_h = 1080                                 # Output resolution of video (HEIGHT)
-fps = 30                                            # Output FPS of video
-vid_directory = sys.argv[1]                         # Directory that stores the videos to edit together
-split_every_n_beat = 8                              # The beat at which clips are split at 
-preset = 'ultrafast'                                # FFMPEG preset to encode the videos
-output_file_name = 'output.mp4'                     # The video file to export
+# audio_path = './music/creativeminds.mp3'            # Path of the song being used
+# output_file_name = 'output.mp4'                     # The video file to export
+# resolution_w = 1920                                 # Output resolution of video (WIDTH)
+# resolution_h = 1080                                 # Output resolution of video (HEIGHT)
+# vids = []                                           # The videos to be stitched together
+# vid_directory = sys.argv[1]                         # Directory that stores the videos to edit together
+# sep = 5                                             # Clips from the same video must be at least this many seconds apart
+# fps = 30                                            # Output FPS of video
+# split_every_n_beat = 8                              # The beat at which clips are split at 
+# preset = 'ultrafast'                                # FFMPEG preset to encode the videos
 
-#
-# Get beat_times and the list of videos
-#
-beat_times = getBeatTimesFromMusic(audio_path, split_every_n_beat)
+def createVideo(
+    audio_path,
+    output_file_name,
+    resolution_w,
+    resolution_h,
+    sep=5,
+    fps=30,
+    split_every_n_beat=4,
+    preset='ultrafast',
+    csv_path='',
+    vids=[],
+    vid_directory=''
+):
+    #
+    # Get beat_times and the list of videos
+    #
+    if csv_path == '':
+        beat_times = getBeatTimesFromMusic(audio_path, split_every_n_beat)
+    else:
+        beat_times = getBeatTimesFromCSV(csv_path, audio_path, 1)
 
-vids = []
-for (dirpath, dirnames, filenames) in walk(vid_directory):
-    vids.extend([dirpath+'/'+name for name in filenames])
 
-#
-# Create the `clips` list, which stores information to split the videos
-# Each index of clips is organized as such:
-# (video_index, start_frame, end_frame, speed_factor)
-#   video_index : int   - The index of the video in `vids`
-#   start_frame : int   - The frame to start cutting the video
-#   end_frame   : int   - The frame to stop cutting the video
-#   speed_factor: float - The factor by which to multiply the PTS of the video to fix speed issues with forcing the input frame rates to `fps`
-#
-clips = []
-tot_frames = 0
-beat_index = 0
-interval = beat_times[beat_index + 1] - beat_times[beat_index]
+    if len(vids) == 0:
+        for (dirpath, dirnames, filenames) in walk(vid_directory):
+            vids.extend([dirpath+'/'+name for name in filenames])
 
-for i in range(len(vids)):
-    if beat_index + 1 >= len(beat_times):
-        break
+    #
+    # Create the `clips` list, which stores information to split the videos
+    # Each index of clips is organized as such:
+    # (video_index, start_frame, end_frame, speed_factor)
+    #   video_index : int   - The index of the video in `vids`
+    #   start_frame : int   - The frame to start cutting the video
+    #   end_frame   : int   - The frame to stop cutting the video
+    #   speed_factor: float - The factor by which to multiply the PTS of the video to fix speed issues with forcing the input frame rates to `fps`
+    #
+    clips = []
+    tot_frames = 0
+    beat_index = 0
+    interval = beat_times[beat_index + 1] - beat_times[beat_index]
 
-    length = getSec(getDuration(vids[i]))
-
-    # Needed to fix speed issues as a result of forcing frame rate to `fps`
-    orig_fps = getFrameRate(vids[i])
-    factor = fps / orig_fps
-
-    cur_time = 0
-
-    # While still enough time left in the video
-    while cur_time+interval < length:
-        cur_time_fn = cur_time * fps
-        interval_fn = interval * fps
-
-        # Correct the frame number if it doesn't match up with the current beat_time
-        correct_frame = int(beat_times[beat_index] * fps)
-        if tot_frames != correct_frame:
-            interval_fn += correct_frame - tot_frames
-
-        clips.append((i, int(cur_time_fn), int(cur_time_fn) + int(interval_fn), factor))
-        tot_frames += int(interval_fn)
-        
-        cur_time = int(cur_time+interval+sep)
-        beat_index += 1
+    for i in range(len(vids)):
         if beat_index + 1 >= len(beat_times):
             break
 
-        interval = beat_times[beat_index + 1] - beat_times[beat_index]
+        length = getSec(getDuration(vids[i]))
 
-#
-# Use filter_complex to combine videos at the specified frame splits 
-#
-cmd = ['ffmpeg']
-cmd.append('-i')
-cmd.append(audio_path)
+        # Needed to fix speed issues as a result of forcing frame rate to `fps`
+        orig_fps = getFrameRate(vids[i])
+        factor = fps / orig_fps
 
-for i in range(len(vids)):
+        cur_time = 0
+
+        # While still enough time left in the video
+        while cur_time+interval < length:
+            cur_time_fn = cur_time * fps
+            interval_fn = interval * fps
+
+            # Correct the frame number if it doesn't match up with the current beat_time
+            correct_frame = int(beat_times[beat_index] * fps)
+            if tot_frames != correct_frame:
+                interval_fn += correct_frame - tot_frames
+
+            clips.append((i, int(cur_time_fn), int(cur_time_fn) + int(interval_fn), factor))
+            tot_frames += int(interval_fn)
+            
+            cur_time = int(cur_time+interval+sep)
+            beat_index += 1
+            if beat_index + 1 >= len(beat_times):
+                break
+
+            interval = beat_times[beat_index + 1] - beat_times[beat_index]
+
+    #
+    # Use filter_complex to combine videos at the specified frame splits 
+    #
+    cmd = ['ffmpeg']
+    cmd.append('-hide_banner')
+    #cmd.append('-loglevel')
+    #cmd.append('error')
+    cmd.append('-i')
+    cmd.append(audio_path)
+
+    for i in range(len(vids)):
+        cmd.append('-r')
+        cmd.append(str(fps))
+        cmd.append('-i')
+        cmd.append(vids[i])
+
+    cmd.append('-filter_complex')
+    filter_str = ''
+    concat_str = ''
+    for i in range(len(clips)):
+        # Add 1 to stream_num because the song is the 0th input
+        stream_num = clips[i][0] + 1
+        factor = clips[i][3]
+        start_pts = clips[i][1]
+        end_pts = clips[i][2]
+        trim_str =  (
+            f'[{stream_num}:v]'
+            f'setpts={factor:.3f}*PTS,'
+            f'trim=start_pts={start_pts}:end_pts={end_pts},'
+            f'setpts=PTS-STARTPTS,'
+            f'scale=w={resolution_w}:h={resolution_h}:force_original_aspect_ratio=decrease'
+            f'[v{i}];'
+        )
+        filter_str += trim_str
+        concat_str += f'[v{i}]'
+    concat_str += f'concat=n={len(clips)}[out]'
+    filter_str += concat_str
+
+    cmd.append(filter_str)
+    cmd.append('-shortest')
+    cmd.append('-map')
+    cmd.append('[out]')
+    cmd.append('-map')
+    cmd.append('0:a')
+    cmd.append('-preset')
+    cmd.append(preset)
     cmd.append('-r')
     cmd.append(str(fps))
-    cmd.append('-i')
-    cmd.append(vids[i])
+    cmd.append('-y')
+    cmd.append(output_file_name)
+    print(cmd)
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    for c in iter(lambda: process.stdout.read(1), b''):  # replace '' with b'' for Python 3
+        c = c.decode('utf-8')
+        #sys.stdout.write(c)
+        if c == 'f':
+            print('LOL')
 
-cmd.append('-filter_complex')
-filter_str = ''
-concat_str = ''
-for i in range(len(clips)):
-    # Add 1 to stream_num because the song is the 0th input
-    stream_num = clips[i][0] + 1
-    factor = clips[i][3]
-    start_pts = clips[i][1]
-    end_pts = clips[i][2]
-    trim_str = f'[{stream_num}:v]\
-                setpts={factor:.3f}*PTS,\
-                trim=start_pts={start_pts}:end_pts={end_pts},\
-                setpts=PTS-STARTPTS,\
-                scale=w={resolution_w}:h={resolution_h}:force_original_aspect_ratio=decrease\
-                [v{i}];'
-    filter_str += trim_str
-    concat_str += f'[v{i}]'
-concat_str += f'concat=n={len(clips)}[out]'
-filter_str += concat_str
+    print('DONE')
 
-cmd.append(filter_str)
-cmd.append('-shortest')
-cmd.append('-map')
-cmd.append('[out]')
-cmd.append('-map')
-cmd.append('0:a')
-cmd.append('-preset')
-cmd.append(preset)
-cmd.append('-r')
-cmd.append(str(fps))
-cmd.append('-y')
-cmd.append(output_file_name)
-subprocess.call(cmd)
-
-print('DONE')
+if __name__ == '__main__':
+    createVideo(
+        './music/creativeminds.mp3',
+        'output.mp4',
+        1920,
+        1080,
+        split_every_n_beat=8,
+        vid_directory=sys.argv[1],
+        csv_path='./creativeminds.csv'
+    )
